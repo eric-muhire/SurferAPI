d86aa85d7338a92d7f9ecd6910992903635fe732
eric-muhire <77967086+eric-muhire@users.noreply.github.com>
2021-04-12 16:07:48 +0200

    Add files via upload
    Initiated a new spring boot web API called surfer

---
+++ src/main/java/com/grupparbete/school/SurferApplication.java
@@ -0,0 +1,13 @@
+package com.grupparbete.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurferApplication.class, args);
+	}
+
+}
