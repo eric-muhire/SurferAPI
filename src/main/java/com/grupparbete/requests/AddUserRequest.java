6f8c1b025dae3d18345a3d1e4bbdfd5da8d58a83
eric-muhire <e.muhire24@gmail.com>
2021-04-23 12:15:37 +0200

    Updated swagger documentation for User entity, controller and users requests.

--- src/main/java/com/grupparbete/requests/AddUserRequest.java
+++ src/main/java/com/grupparbete/requests/AddUserRequest.java
@@ -1,8 +1,19 @@
 package com.grupparbete.requests;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Add new User Request")
+
 public class AddUserRequest {
+
+    @ApiModelProperty(notes = "The id of the user")
     private int id;
+
+    @ApiModelProperty(notes = "The name of the user")
     private String userName;
+
+    @ApiModelProperty(notes = "The user´s  e-mail")
     private String userEmail;
 
     public int getId() {
