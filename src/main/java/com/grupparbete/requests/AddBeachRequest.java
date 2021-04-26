4027bdbdf1626cc47ee2cf3891252409c53a3703
MousaabYedjour <mousaab.yedjour@gmail.com>
2021-04-26 15:50:13 +0200

    Updated beachID from int to UUID

--- src/main/java/com/grupparbete/requests/AddBeachRequest.java
+++ src/main/java/com/grupparbete/requests/AddBeachRequest.java
@@ -3,18 +3,20 @@ package com.grupparbete.requests;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.util.UUID;
+
 @ApiModel(description = "Add a new beach request")
 public class AddBeachRequest {
     @ApiModelProperty(notes = "The ID number of the beach")
-    private int id;
+    private UUID id;
     @ApiModelProperty(notes = "Name of the beach")
     private String beachName;
 
-    public int getId() {
+    public UUID getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(UUID id) {
         this.id = id;
     }
 
