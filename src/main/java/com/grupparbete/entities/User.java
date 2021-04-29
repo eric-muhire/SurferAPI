952d0dd34b2f7bbe90863dbcc8e8c287470a9c27
Moburger <jfmoberg@me.com>
2021-04-29 21:19:13 +0200

    user lombok

--- src/main/java/com/grupparbete/entities/User.java
+++ src/main/java/com/grupparbete/entities/User.java
@@ -2,36 +2,19 @@ package com.grupparbete.entities;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @ApiModel(description = "This is the User Model",value ="User model")
 
+@Data
+@NoArgsConstructor
 public class User {
     @ApiModelProperty(notes = "Unique identifier for User")
     private int userId;
     private String userName;
     private String userEmail;
 
-    public User() {
-
-    }
-    public int getUserId() {
-        return userId;
-    }
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-    public String getUserName() {
-        return userName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-    public String getUserEmail() {
-        return userEmail;
-    }
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
 }
 
 
