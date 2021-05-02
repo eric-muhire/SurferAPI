dc23681f2a72a5d1f239d2b17012ff5864c2b5eb
eric-muhire <e.muhire24@gmail.com>
2021-05-02 21:36:18 +0200

    -I have changed UID in post entity to a long id. -Have updated @APiModelProperty in in post entity.

--- src/main/java/com/grupparbete/entities/Post.java
+++ src/main/java/com/grupparbete/entities/Post.java
@@ -3,12 +3,9 @@ package com.grupparbete.entities;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.Date;
-import java.util.UUID;
 
 @ApiModel(description = "This is the post model",
         value = "Post Model")
@@ -16,12 +13,18 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Post {
     @ApiModelProperty(notes = "Unique identifier for post")
-    private UUID id;
+    private long id;
+    @ApiModelProperty(notes = "Information about weather")
     private String weather;
+    @ApiModelProperty(notes = "Information about waves")
     private String waves;
+    @ApiModelProperty(notes = "Unique identifier for beach")
     private int beachId;
+    @ApiModelProperty(notes = "The date when a post was created")
     private Date createdAt;
+    @ApiModelProperty(notes = "The date when a post was last updated")
     private Date updatedAt;
+    @ApiModelProperty(notes = "Unique identifier for user")
     private int userId;
 
 }
